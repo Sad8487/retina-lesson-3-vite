6f95621c991597d94506037bfdc62823cb8d8d3e
Sad8487 <svetlana.dyablo@gmail.com>
2024-12-12 18:15:01 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+import image from './assets/images/image.jpg';
+
+
+
+function App() {
+    return (
+        <div className="App">
+            <p>Hello, Retina &#128075;</p>
+            {/* <div className={'bg'}></div> */}
+            {/* <img src={image} alt="nature"/> */}
+        </div>
+    );
+}
+
+export default App;
+
+
